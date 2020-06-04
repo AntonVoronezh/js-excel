0230dc0db5041917c71a345d1d7b2a3623ad8f93
AntonVoronezh <pstut@mail.ru>
2020-06-04 14:40:36 +0300

    вебпак

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,35 @@
 const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     // context: path.resolve(__dirname, 'src'),
     mode: "development",
     entry: './src/index.js',
     output: {
-        filename: "bundle.js",
+        filename: "bundle.[hash].js",
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': './src',
+            '@core': './src/core'
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new CopyWebpackPlugin({
+            from: './src/favicon.ico',
+            to: 'dist'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.[hash].css'
+        })
+    ]
 }
