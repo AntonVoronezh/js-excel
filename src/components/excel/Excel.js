b0eb8c0986aae526a5ea8cbf2e255278cbf89584
AntonVoronezh <pstut@mail.ru>
2020-06-08 18:35:19 +0300

    шаблон

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,6 +1,23 @@
+// eslint-disable-next-line require-jsdoc
 export class Excel {
-    constructor(selector, options) {
-        this.$el = document.querySelector(selector);
-        this.components = options.components || [];
-    }
+  constructor(selector, options) {
+    this.$el = document.querySelector(selector);
+    this.components = options.components || [];
+  }
+
+  getRoot() {
+    const $root = document.createElement('div');
+
+    this.components.forEach((Component) => {
+      const component = new Component();
+      $root.insertAdjacentHTML('beforeend', component.toHTML());
+    });
+
+
+    return $root;
+  }
+
+  render() {
+    this.$el.append(this.getRoot());
+  }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,7 +1,7 @@
-import {DomListener} from '@core/DomListener';
+import { DomListener } from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
-  //возвращает шаблон компонетна
+  // возвращает шаблон компонетна
   toHTML() {
     return '';
   }
