a3e8bd17d53477e21cf65df22ef9613ffa555a2c
AntonVoronezh <pstut@mail.ru>
2020-06-10 16:52:26 +0300

    шаблон

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -16,11 +16,17 @@ export class DomListener {
         throw new Error(`Method ${method} is not implemented in ${this.name || ''} component`);
       }
 
-      this.$root.on(listener, this[method].bind(this));
+      this[method] = this[method].bind(this);
+      this.$root.on(listener, this[method]);
     });
   }
 
-  removeDOMListeners() {}
+  removeDOMListeners() {
+    this.listeners.forEach((listener) => {
+      const method = getMethodName(listener);
+      this.$root.off(listener, this[method]);
+    });
+  }
 }
 
 function getMethodName(eventName) {
