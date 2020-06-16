9e121b4ac4d60393ff6a607640cda52647747d4a
AntonVoronezh <pstut@mail.ru>
2020-06-16 18:39:28 +0300

    ресайз

--- src/core/dom.js
+++ src/core/dom.js
@@ -45,6 +45,18 @@ class Dom {
   getCoords() {
     return this.$el.getBoundingClientRect();
   }
+
+  get data() {
+    return this.$el.dataset;
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector);
+  }
+
+  css(styles = {}) {
+    Object.keys(styles).forEach((key) => (this.$el.style[key] = styles[key]));
+  }
 }
 
 export function $(selector) {
