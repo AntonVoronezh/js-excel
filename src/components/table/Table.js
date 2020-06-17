de51b36474342908868a8674a5e0354bfc65c09f
AntonVoronezh <pstut@mail.ru>
2020-06-17 18:05:56 +0300

    ресайз

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,15 +1,28 @@
 import { ExcelComponent } from '@core/ExcelComponent';
 import { createTable } from '@/components/table/table.template';
 import { resizeHandler } from '@/components/table/table.resize';
-import { shouldRisize } from '@/components/table/table.functions';
+import {isCell, matrix, nextSelector, shouldRisize} from '@/components/table/table.functions';
+import { TableSelection } from '@/components/table/TableSelection';
+import { $ } from '@core/dom';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
   constructor($root) {
     super($root, {
-      listeners: ['mousedown'],
+      listeners: ['mousedown', 'keydown'],
     });
+  }
+
+  prepare() {
+    this.selection = new TableSelection();
+  }
+
+  init() {
+    super.init();
+    const $cell = this.$root.find('[data-id="0:0"]');
+
+    this.selection.select($cell);
   }
 
   toHTML() {
@@ -19,6 +32,30 @@ export class Table extends ExcelComponent {
   onMousedown(event) {
     if (shouldRisize(event)) {
       resizeHandler(this.$root, event);
+    } else if (isCell(event)) {
+      const $target = $(event.target);
+
+      if (event.shiftKey) {
+        const target = $target.id(true);
+        const current = this.selection.current.id(true);
+
+        const $cells = matrix(target, current).map((id) => this.$root.find(`[data-id="${id}"]`));
+        this.selection.selectGroup($cells);
+      } else {
+        this.selection.select($target);
+      }
+    }
+  }
+
+  onKeydown(event) {
+    const keys = ['Enter', 'Tab', 'ArrowLeft', 'ArrowRight', 'ArrowDown', 'ArrowUp'];
+    const { key } = event;
+
+    if (keys.includes(key) && !event.shiftKey) {
+      event.preventDefault();
+      const { col, row } = this.selection.current.id(true);
+      const $next = this.$root.find(nextSelector(key, col, row));
+      this.selection.select($next);
     }
   }
 }
