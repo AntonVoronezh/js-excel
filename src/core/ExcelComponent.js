398ebdec9918655292805f192d5453f5436720eb
AntonVoronezh <pstut@mail.ru>
2020-06-18 17:59:49 +0300

    ресайз

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,6 +4,8 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || '';
+    this.emitter = options.emitter;
+    this.unsubscribers = [];
 
     this.prepare();
   }
@@ -15,11 +17,21 @@ export class ExcelComponent extends DomListener {
     return '';
   }
 
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args);
+  }
+
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn);
+    this.unsubscribers.push(unsub);
+  }
+
   init() {
     this.initDOMListeners();
   }
 
   destroy() {
+    this.unsubscribers.forEach((unsub) => unsub());
     this.removeDOMListeners();
   }
 }
