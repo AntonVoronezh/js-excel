1237fd4c37cbd7115bc815964b3c7bf44ec4ab1a
AntonVoronezh <pstut@mail.ru>
2020-06-19 19:04:36 +0300

    состояние

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,16 @@
+export function rootReducer(state, action) {
+  let prevState;
+
+  switch (action.type) {
+    case 'TABLE_RESIZE':
+      prevState = state.colState || {};
+      prevState[action.data.id] = action.data.value;
+
+      return {
+        ...state,
+        colState: prevState,
+      };
+    default:
+      return state;
+  }
+}
