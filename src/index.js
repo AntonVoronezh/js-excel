a5a76d7caa2aecd074df2ef82b34c36df55dbbb8
AntonVoronezh <pstut@mail.ru>
2020-06-19 19:04:40 +0300

    состояние

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import { Header } from '@/components/header/Header';
 import { Toolbar } from '@/components/toolbar/Toolbar';
 import { Formula } from '@/components/formula/Formula';
 import { Table } from '@/components/table/Table';
+import { createStore } from '@core/createStore';
+import { rootReducer } from '@/redux/rootReducer';
+
+const store = createStore(rootReducer, {
+  colState: {},
+});
 
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
+  store,
 });
 
 excel.render();
