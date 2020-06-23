e5a93d63cbadd5b161ba2c33629434405e6636fc
AntonVoronezh <pstut@mail.ru>
2020-06-23 18:49:13 +0300

    состояние

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,16 +1,28 @@
-import { TABLE_RESIZE } from '@/redux/types';
+import { CHANGE_TEXT, TABLE_RESIZE } from '@/redux/types';
 
 export function rootReducer(state, action) {
   let prevState;
+  let field;
 
   switch (action.type) {
     case TABLE_RESIZE:
-      prevState = state.colState || {};
+      field = action.data.type === 'col' ? 'colState' : 'rowState';
+      prevState = state[field] || {};
       prevState[action.data.id] = action.data.value;
 
       return {
         ...state,
-        colState: prevState,
+        [field]: prevState,
+      };
+
+    case CHANGE_TEXT:
+      prevState = state['dataState'] || {};
+      prevState[action.data.id] = action.data.value;
+      console.log(action.data.id);
+      return {
+        ...state,
+        currentText: action.data.value,
+        dataState: prevState,
       };
     default:
       return state;
