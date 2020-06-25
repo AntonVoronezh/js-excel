67d39f61bc7ce94bbddbf00126b8f1c7b10dada6
AntonVoronezh <pstut@mail.ru>
2020-06-25 18:49:59 +0300

    состояние

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,16 +1,43 @@
-import { ExcelComponent } from '@core/ExcelComponent';
+import { createToolbar } from '@/components/toolbar/toolbar.template';
+import { $ } from '@core/dom';
+import { ExcelStateComponent } from '@core/ExcelStateComponent';
 
-export class Toolbar extends ExcelComponent {
+export class Toolbar extends ExcelStateComponent {
   static className = 'excel__toolbar';
 
   constructor($root, options) {
     super($root, {
       name: 'Toolbar',
+      listeners: ['click'],
       ...options,
     });
   }
 
+  prepare() {
+    const initialState = {
+      textAlign: 'left',
+      fontWeight: 'normal',
+      fontStyle: 'normal',
+      textDecoration: 'none',
+    };
+
+    this.initState(initialState);
+  }
+
   toHTML() {
-    return '<h1>Toolbar</h1>';
+    return this.template;
+  }
+
+  get template() {
+    return createToolbar(this.state);
+  }
+
+  onClick(event) {
+    const $target = $(event.target);
+    if ($target.data.type === 'button') {
+      const value = JSON.parse($target.data.value);
+      const key = Object.keys(value)[0];
+      this.setState({ [key]: value[key] });
+    }
   }
 }
