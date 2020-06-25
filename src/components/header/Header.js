91acb09ccfe6ae97101b75361a693b09ea5d1659
AntonVoronezh <pstut@mail.ru>
2020-06-25 18:49:50 +0300

    состояние

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,6 +11,17 @@ export class Header extends ExcelComponent {
   }
 
   toHTML() {
-    return '<h1>Header</h1>';
+    return `
+     <input type="text" class="input" value="Новая таблица" />
+
+          <div>
+            <div class="button">
+              <i class="material-icons">delete</i>
+            </div>
+
+            <div class="button">
+              <i class="material-icons">exit_to_app</i>
+            </div>
+          </div>`;
   }
 }
