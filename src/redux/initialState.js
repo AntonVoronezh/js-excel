e82df87f6d1ea5ea0ce1459ff9b1b82e2254bacf
AntonVoronezh <pstut@mail.ru>
2020-06-26 17:54:47 +0300

    состояние

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,10 +1,16 @@
 import { storage } from '@core/utils';
+import { defaultStyles, defaultTitle } from '@/constatnts';
 
 const defaultState = {
+  title: defaultTitle,
   rowState: {},
   colState: {},
   dataState: {},
+  stylesState: {},
   currentText: '',
+  currentStyles: defaultStyles,
 };
 
-export const initialState = storage('excel-state') ? storage('excel-state') : defaultState;
+const normalize = (state) => ({ ...state, currentText: '', currentStyles: defaultStyles });
+
+export const initialState = storage('excel-state') ? normalize(storage('excel-state')) : defaultState;
