3bb6addb18c6d99dc77da4e3945f903f7711e3ff
AntonVoronezh <pstut@mail.ru>
2020-06-26 17:54:14 +0300

    состояние

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,7 @@
 import { ExcelComponent } from '@core/ExcelComponent';
+import { $ } from '@core/dom';
+import { changeTitle } from '@/redux/actions';
+import { defaultTitle } from '@/constatnts';
 
 export class Header extends ExcelComponent {
   static className = 'excel__header';
@@ -6,13 +9,16 @@ export class Header extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Header',
+      listeners: ['input'],
       ...options,
     });
   }
 
   toHTML() {
+    const title = this.store.getState().title || defaultTitle;
+
     return `
-     <input type="text" class="input" value="Новая таблица" />
+     <input type="text" class="input" value="${title}" />
 
           <div>
             <div class="button">
@@ -23,5 +29,10 @@ export class Header extends ExcelComponent {
               <i class="material-icons">exit_to_app</i>
             </div>
           </div>`;
+  }
+
+  onInput(event) {
+    const $target = $(event.target);
+    this.$dispatch(changeTitle($target.text()));
   }
 }
