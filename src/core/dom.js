7e29e0704d08dd73f69d61da0689744f4c390156
AntonVoronezh <pstut@mail.ru>
2020-06-26 17:54:32 +0300

    состояние

--- src/core/dom.js
+++ src/core/dom.js
@@ -12,7 +12,7 @@ class Dom {
   }
 
   text(text) {
-    if (typeof text === 'string') {
+    if (typeof text !== 'undefined') {
       this.$el.textContent = text;
       return this;
     }
@@ -98,6 +98,22 @@ class Dom {
   focus() {
     this.$el.focus();
     return this;
+  }
+
+  attr(name, value) {
+    if (value) {
+      this.$el.setAttribute(name, value);
+      return this;
+    }
+
+    return this.$el.getAttribute(name);
+  }
+
+  getStyles(styles = []) {
+    return styles.reduce((res, s) => {
+      res[s] = this.$el.style[s];
+      return res;
+    }, {});
   }
 }
 
