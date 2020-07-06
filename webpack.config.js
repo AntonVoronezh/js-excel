2260dfc33900389a70b2da1863ed997915f35495
AntonVoronezh <pstut@mail.ru>
2020-07-06 10:06:26 +0300

    состояние

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
@@ -10,15 +11,7 @@ const isDev = !isProd;
 const filename = (ext) => (isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`);
 
 const jsLoaders = () => {
-  const loaders = [
-    {
-      loader: "babel-loader",
-      options: {
-        presets: ["@babel/preset-env"],
-        plugins: ["@babel/plugin-proposal-class-properties"]
-      },
-    },
-  ];
+  const loaders = ["babel-loader"];
 
   if (isDev) {
     loaders.push("eslint-loader");
@@ -66,6 +59,9 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: filename("css"),
+    }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
     }),
   ],
   module: {
